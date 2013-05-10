82baec84c1e30d0c4825bacfd3a68ad5b06084a9
Per_ine <perine@ubuntu.ubuntu-domain>
2013-05-10 10:33:38 +0200

    Editing relationships between tables

--- fmt/models.py
+++ fmt/models.py
@@ -6,19 +6,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Location(models.Model):
-
-    ip_address = models.IPAddressField(_('IP Address'), unique=True)
-    hostname = models.CharField('Resolved hostname', max_length=255, blank=True)
-    zipcode = models.CharField(_('ZIP Code'), max_length=64, blank=True)
-
-    class Meta:
-        verbose_name = _('Location')
-        verbose_name_plural = _('Locations')
-
-    def __unicode__(self):
-        return unicode(self.ip_address)
-
 class Main_Frame(models.Model):
     mdf = models.CharField(_('Main Frame'), max_length=50, blank=True)
     lon = models.FloatField(_('Longitude'), blank=True, null=True)
@@ -30,7 +17,24 @@ class Main_Frame(models.Model):
     def __unicode__(self):
         return unicode(self.mdf)
 
+
+class Location(models.Model):
+    user = models.ForeignKey(User)
+    main_frame = models.ForeignKey(Main_Frame)  
+    ip_address = models.IPAddressField(_('IP Address'), unique=True)
+    hostname = models.CharField('Resolved hostname', max_length=255, blank=True)
+    zipcode = models.CharField(_('ZIP Code'), max_length=64, blank=True)
+
+    class Meta:
+        verbose_name = _('Location')
+        verbose_name_plural = _('Locations')
+
+    def __unicode__(self):
+        return unicode(self.ip_address)
+
+
 class Video(models.Model):
+    location = models.ForeignKey(Location)
     video_title = models.CharField(max_length=255, verbose_name=_("YouTube Video Title"), null=True, blank=True)
     video_url = models.URLField(verbose_name=_("YouTube Video URL"), null=True, blank=True)
     #startDate = models.DateTimeField("Start date")
@@ -43,10 +47,9 @@ class Video(models.Model):
     def __unicode__(self):
         return unicode(self.video_title)
 
+
 class Load(models.Model):
     video = models.ForeignKey(Video)
-    user = models.ForeignKey(User)
-    location = models.ForeignKey(Location)
     minimum = models.IntegerField(verbose_name=_("Minimum"))
     maximum = models.IntegerField(verbose_name=_("Maximum"))
     average = models.IntegerField(verbose_name=_("Average"))
