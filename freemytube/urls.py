0b93867e09d75f8e34e28e1e5a275561b0394c9d
Per_ine <perine@ubuntu.ubuntu-domain>
2013-05-12 22:56:05 +0200

    Editing urls.py *Hooking up the resource *Adding to the API

--- freemytube/urls.py
+++ freemytube/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import patterns, include, url
+from tastypie.api import Api
+from core.api import MainFrameResource, UserResource, LocationResource, VideoResource, MeasurementResource
 
-# Uncomment the next two lines to enable the admin:
+# Uncomment the next two lines to enable the admiUserResource, LocationResource, VideoResource, MeasurementResourcen:
 from django.contrib import admin
 admin.autodiscover()
+
+v1_api = Api(api_name='freemytube')
+v1_api.register(MainFrameResource())
+v1_api.register(UserResource())
+v1_api.register(LocationResource())
+v1_api.register(VideoResource())
+v1_api.register(MeasurementResource())
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,4 +23,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    (r'^api/', include(v1_api.urls)),
 )
