3eb9e0d190afd7682e6af6ded67bd15f8b9f85af
Per_ine <perine@ubuntu.ubuntu-domain>
2013-05-12 22:42:52 +0200

    EDiting the model of the class: MainFrame

--- core/models.py
+++ core/models.py
@@ -8,6 +8,10 @@ from django.contrib.auth.models import User
 
 class MainFrame(models.Model):
     name = models.CharField(_('Main Frame'), max_length=50, blank=False)
+    town = models.CharField(_('Town Name'), max_length=50, blank=True)
+    nra = models.CharField(_('NRA / NRO Name'), max_length=50, blank=True)
+    dslam = models.CharField(_('Dslam / Switch Name'), max_length=50, blank=True)
+    ip_adress = models.IPAddressField(_('Dslam / Switch IP Address'), unique=True)
 
     class Meta:
         verbose_name = _('Main frame')
