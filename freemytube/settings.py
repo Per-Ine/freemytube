d3c7fbf6467e2c95f84192ce5b7dfff1f6a12d5a
Per_ine <perine@ubuntu.ubuntu-domain>
2013-05-13 12:27:35 +0200

    Init before REST framework

--- freemytube/settings.py
+++ freemytube/settings.py
@@ -123,7 +123,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'tastypie',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- freemytube/urls.py
+++ freemytube/urls.py
@@ -1,17 +1,9 @@
 from django.conf.urls import patterns, include, url
-from tastypie.api import Api
 from core.api import MainFrameResource, UserResource, LocationResource, VideoResource, MeasurementResource
 
 # Uncomment the next two lines to enable the admiUserResource, LocationResource, VideoResource, MeasurementResourcen:
 from django.contrib import admin
 admin.autodiscover()
-
-freemytube_api = Api(api_name='freemytube')
-freemytube_api.register(MainFrameResource())
-freemytube_api.register(UserResource())
-freemytube_api.register(LocationResource())
-freemytube_api.register(VideoResource())
-freemytube_api.register(MeasurementResource())
 
 urlpatterns = patterns('',
     # Examples:
@@ -23,5 +15,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    (r'^api/', include(freemytube_api.urls)),
 )
