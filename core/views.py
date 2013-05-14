c67016cccc63f19659c29cc6fd2a13533c591bdb
Per_ine <perine@ubuntu.ubuntu-domain>
2013-05-14 09:49:34 +0200

    Adding and editing files using the REST framework.

--- core/views.py
+++ core/views.py
@@ -1,1 +1,60 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from core.models import MainFrame
+from core.serializers import MainFrameSerializer
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders it's content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+    def frame_list(request):
+    """
+    List all code main frame, or create a new main frame.
+    """
+    if request.method == 'GET':
+        frame = MainFrame.objects.all()
+        serializer = MainFrameSerializer(frame, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        else:
+            return JSONResponse(serializer.errors, status=400)
+
+    def frame(request, pk):
+    """
+    Retrieve, update or delete a code main frame.
+    """
+    try:
+        frame = MainFrame.objects.get(pk=pk)
+    except MainFrame.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(frame)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = MainFrameSerializer(frame, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        else:
+            return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        frame.delete()
+        return HttpResponse(status=204)
--- freemytube/urls.py
+++ freemytube/urls.py
@@ -1,11 +1,24 @@
 from django.conf.urls import patterns, include, url
-from core.api import MainFrameResource, UserResource, LocationResource, VideoResource, MeasurementResource
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, routers
 
 # Uncomment the next two lines to enable the admiUserResource, LocationResource, VideoResource, MeasurementResourcen:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    model = User
+
+class GroupViewSet(viewsets.ModelViewSet):
+    model = Group
+
+# Routers provide an easy way of automatically determining the URL conf
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = patterns('freemytube.views',
     # Examples:
     # url(r'^$', 'freemytube.views.home', name='home'),
     # url(r'^freemytube/', include('freemytube.foo.urls')),
@@ -15,4 +28,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^freemytube/$', 'frame_list'),
+    url(r'^freemytube/(?P<pk>[0-9]+)/$', 'frame_detail'),
 )
