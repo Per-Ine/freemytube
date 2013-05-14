d4abe603f4de250b8583ec37b72bd5a67e6a8320
Julien Maupetit <julien@maupetit.net>
2013-05-14 16:42:44 +0200

    First working example of a custom API

--- freemytube/urls.py
+++ freemytube/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from rest_framework import viewsets, routers
 
+from core.views import ListMainFrames
+
 # Uncomment the next two lines to enable the admiUserResource, LocationResource, VideoResource, MeasurementResourcen:
 from django.contrib import admin
 admin.autodiscover()
@@ -20,5 +22,6 @@ urlpatterns = patterns('',
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^mainFrame/$', 'core.views.frame_list'),
+    url(r'^mainFrame2/$', ListMainFrames.as_view()),
     url(r'^mainFrame/(?P<pk>[0-9]+)/$', 'core.views.frame_details'),
 )
